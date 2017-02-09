7ac89f3c899be4bd08026d6511956021f211edfa
Eugene Sia <eugene@eugenesia.co.uk>
2017-02-09 12:28:24 +0000

    Restart bot process if it dies

--- run_slack_bot.sh
+++ run_slack_bot.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+# Run Slack bot and restart if it dies.
+# See http://stackoverflow.com/questions/696839/how-do-i-write-a-bash-script-to-restart-a-process-if-it-dies
 
-# Set your Slack token and Api.ai token here.
-# Then run using nohup run_slack_bot.js &
+#
+# # Kill prev process
+# pkill -f 'nodemon slack_bot.js'
+# pkill -f 'node slack_bot.js'
+# 
+# # Run it again
+# # Nodemon watches for code changes and restarts process.
+# nodemon slack_bot.js &
+# 
 
-# Kill prev process
-pkill -f 'nodemon slack_bot.js'
-pkill -f 'node slack_bot.js'
-
-# Run it again
-nodemon slack_bot.js &
+until nodemon slack_bot.js; do
+  echo "Eugenebot crashed with exit code $?.  Respawning.." >&2
+  sleep 1
+done
 
