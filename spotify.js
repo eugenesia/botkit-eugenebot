b0905b70bc7a6136808d6dbe4786feb641db19ec
Eugene Sia <eugene@eugenesia.co.uk>
2017-07-14 12:00:35 +0000

    Get and set Spotify access token after auth

--- spotify.js
+++ spotify.js
@@ -16,6 +16,19 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: config.spotify.redirectUri,
 });
 
+// Retrieve an access token
+// This is required as Spotify implemented a new auth flow since May 2017.
+// See https://developer.spotify.com/news-stories/2017/01/27/removing-unauthenticated-calls-to-the-web-api/
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  });
 
 // Search tracks whose name, album or artist contains a query term.
 // done: function(err, tracks)
